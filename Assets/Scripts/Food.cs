4eafa37325dee1229f89d2713bd6740b9af15d8e
Halloul Tarek <halloultarek1@gmail.com>
2022-07-22 01:12:07 +0100

    Initial commit

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public BoxCollider2D gridArea;
+
+    private void Start()
+    {
+        RandomizePosition();
+    }
+    public void RandomizePosition()
+    {
+        Bounds bounds = this.gridArea.bounds;
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collided_Obj)
+    {
+        if (collided_Obj.tag=="Player")
+        {
+            RandomizePosition();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Snake
+ A simple Snake Game Project for beginners.
